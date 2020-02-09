ec663c9dcfb8cefa11ff13bba85eb6f4fda4bf43
ReneSkukies <57703446+ReneSkukies@users.noreply.github.com>
2020-02-09 20:24:57 +0100

    bad_chan function fix + more
    Function for detecting bad channels fixed, so it now takes epoched data.
    
    Additionally fixed some parts of the pipeline:
    - Epochs are baselined in the tms-interpolation function
    - Interpolation of channels fixed
    - etc.
    
    Runs now until butterfly-plot is displayed

--- EEG_TMS_Pipeline_090220.py
+++ EEG_TMS_Pipeline_090220.py
@@ -9,10 +9,19 @@ from scipy.integrate import simps
 
 # define path and filename (here you might want to loop over datasets!)
 filename = "ane_SD_EMG_1016_awake_tms.vhdr"
-filepath = Path("C:/Users/imadjb/Documents/EEG_ANALYSIS/ane_SD_1016")
+#filepath = Path("C:/Users/imadjb/Documents/EEG_ANALYSIS/ane_SD_1016")
+filepath = Path("E:/Anesthesia/EEG/ane_SD_1016")
 file = filepath / filename
 
+# function for pausing for loop, works only in windows!!!
+# def wait():
+#     end_flag = 0
+#     while not end_flag:
+#         x = m.getch()
+#         if m.getch():
+#             end_flag = 1
 
+        
 def plot_response(signal, argument):
     """plot response to check what happened with the data"""
     if "time" in argument:
@@ -29,35 +38,50 @@ def detect_bad_ch(eeg):
     """plots each channel so user can decide whether good (mouse click) or bad (enter / space)"""
     good_ch, bad_ch = [], []
     intvl = eeg.__len__() // 20
+    if type(eeg) is mne.epochs.EpochsArray:
+        # Benny's way is way too slow.... and a bit ugly...         
+        # Let's try it MNE style
+        n_chan = eeg.ch_names.__len__()
+        n_disp = 4
+        for i in range(0,n_chan,n_disp):
+            # Choose 4 channels at a time
+            cur_picks = eeg.ch_names[i:(i + n_disp)]
+            fig = eeg.plot(picks=cur_picks, title='Click channel names to reject, click on epochs to reject epoch; Use mouse to move around figure, press any key to advance')
 
-    for ch in eeg.ch_names:
-        """loop over each channel and plot to decide if bad"""
-        time_data = eeg[eeg.ch_names.index(ch)][0][0]
-        df = pd.DataFrame()
+            # Wait until keyboard is pressed
+            while not plt.waitforbuttonpress():            
+                print('Inspecting channels..')
 
-        for i in range(20):
-            df_window = pd.DataFrame(time_data[i * intvl:(i + 1) * intvl])
-            df_window += (i + 1) * 0.0001
-            df = pd.concat((df, df_window), axis=1)
-
-        df *= 1000  # just for plotting
-        fig = plt.figure(figsize=(14, 8))
-        fig.suptitle(f"{ch}: mouse click for keep (good), any other key for remove (bad)")
-        ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=1)
-        ax2 = plt.subplot2grid((3, 3), (0, 1), colspan=2, rowspan=3)
-        ax1.psd(time_data, 5000, 5000)
-        ax1.set_xlim([0, 55])
-        ax2.plot(df, 'b')
-        plt.show()
-
-        if not plt.waitforbuttonpress():
-            good_ch.append(ch)
             plt.close(fig)
-        else:
-            bad_ch.append(ch)
-            plt.close(fig)
+        return eeg
+    else:
+        for ch in eeg.ch_names:
+            """loop over each channel and plot to decide if bad"""
+            time_data = eeg[eeg.ch_names.index(ch)][0][0]
+            df = pd.DataFrame()
+            for i in range(20):
+                df_window = pd.DataFrame(time_data[i * intvl:(i + 1) * intvl])
+                df_window += (i + 1) * 0.0001
+                df = pd.concat((df, df_window), axis=1)
 
-    return good_ch, bad_ch
+            df *= 1000  # just for plotting
+            fig = plt.figure(figsize=(14, 8))
+            fig.suptitle(f"{ch}: mouse click for keep (good), any other key for remove (bad)")
+            ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=1)
+            ax2 = plt.subplot2grid((3, 3), (0, 1), colspan=2, rowspan=3)
+            ax1.psd(time_data, 5000, 5000)
+            ax1.set_xlim([0, 55])
+            ax2.plot(df, 'b')
+            plt.show()
+
+            if not plt.waitforbuttonpress():
+                good_ch.append(ch)
+                plt.close(fig)
+            else:
+                bad_ch.append(ch)
+                plt.close(fig)
+
+        return good_ch, bad_ch
 
 
 def detect_bad_ic(ica_data, data_orig):
@@ -81,11 +105,13 @@ def detect_bad_ic(ica_data, data_orig):
 
 
     
-
+# Make feature that asks for parameters by inspecting the data
 def TMS_interpolate(data,tmin=-0.2,tmax=0.5,fsample = 5000, pulse_start = -0.002, pulse_end = 0.007): # interpolates TMS pulses
     events = mne.events_from_annotations(data) #Generate an event file for pulses and annotations:
 
     #Epoch data using function (setting index 0 for events selects a list in the array obj. events):
+    "Baseline correct??"
+    baseline = (None, 0.0)
     epochs = mne.Epochs(data, events[0], tmin=tmin, tmax=tmax, preload=True)
     #epochs.plot()
 
@@ -110,7 +136,7 @@ def TMS_interpolate(data,tmin=-0.2,tmax=0.5,fsample = 5000, pulse_start = -0.002
     raw_epochs_interpolated = raw_epochs.copy()
     raw_epochs_interpolated[:,:,int(pulse_start_datapoint):int(pulse_end_datapoint)] = new_pulse
 
-    newepochs = mne.EpochsArray(raw_epochs_interpolated,info=epochs.info, tmin=tmin)
+    newepochs = mne.EpochsArray(raw_epochs_interpolated,info=epochs.info, tmin=tmin, baseline = baseline)
     return newepochs
 
     #plt.figure()
@@ -124,30 +150,27 @@ data = mne.io.read_raw_brainvision(file)
 data.load_data()
 # plot_response(data, 'time')
 
-# 2. remove bad channels (or do not remove but track them)
-good, bad = detect_bad_ch(data)
-data.info['bads'] = bad  # keep track of bad channels but do not remove (MNE style)
-data.drop_channels(bad)  # remove bad channels (eeglab style)
+# 1.1. channel info (remove EMG and set type for EOG channels)
+#data.drop_channels('EMG')
+data.set_channel_types({'VEOG': 'eog', 'HEOG': 'eog'})
+data.set_montage('standard_1005')
+
+# 2. Replace TMS pulse artefact with noise based on baseline statistics
+data = TMS_interpolate(data) # look at function to change
+# data.apply_baseline(None,0.0) ; Done in function now
+
+# 3. remove bad channels (or do not remove but track them)
+#good, bad = detect_bad_ch(data)
+data = detect_bad_ch(data)
+bad = data.info['bads']  # keep track of bad channels but do not remove (MNE style)
+#data.drop_channels(bad)  # remove bad channels (eeglab style)
 data = data.interpolate_bads(reset_bads=True)  # for presentation of bad channels change to False
-
-
-# 3. Replace TMS pulse artefact with noise based on baseline statistics
-
-data = TMS_interpolate(data)
 
 # 4. resample (with low-pass filter!)
 new_sampling = 1000
 data.resample(new_sampling, npad='auto')
 #plot_response(data, ['time', 'psd'])
 
-# 5. channel info (remove EMG and set type for EOG channels)
-#data.drop_channels('EMG')
-data.set_channel_types({'VEOG': 'eog', 'HEOG': 'eog'})
-data.set_montage('standard_1005')
-
-
-
-# 6. Replace data around TMS pulse with constant amplitude data? (research)
 
 # 7. PCA + ICA (by default if rank violated)
 n_ic = len(data.ch_names)-len(bad)
@@ -164,24 +187,20 @@ ica.apply(clean_data, exclude=ica.exclude)
 
 # 10. filter (first high- then low-pass; notch-filter?)
 l_cut, h_cut = 1, 80
-data.filter(l_freq=l_cut, h_freq=h_cut)
+clean_data.filter(l_freq=l_cut, h_freq=h_cut)
 # plot_response(data, 'psd')
 
 # 11. Replace data around TMS pulse with constant amplitude data? (research)
 
 # 12. Run ICA again to remove any remaining artifacts.
 
-# 13. interpolate channels (spherical spline method recommended)
-data = data.interpolate_bads(reset_bads=True)  # for presentation of bad channels change to False
-# plot_response(data, 'butter')
-
 # 14. re-reference to average
-data.set_eeg_reference('average', projection=False)  # you might want to go with True
+clean_data.set_eeg_reference('average', projection=False)  # you might want to go with True
 
 
 # (15). create evoked data (average over all trials)/ Butterfly plot
 
-evoked_epochs = data.average()
+evoked_epochs = clean_data.average()
 evoked_epochs.plot_joint() # plots butterfly plot
 
 # remove line-noise by notch filter (not always recommended!)
